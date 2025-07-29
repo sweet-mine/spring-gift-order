90a66ed234732b3bd139ae9eb9e21c65edc0a9cd
sweet-mine <dltlgos@naver.com>
2025-07-29 16:40:54 +0900

    카카오 토큰 발급 및 사용자 정보 반환 버그 수정

--- src/main/java/gift/configuration/AppConfig.java
+++ src/main/java/gift/configuration/AppConfig.java
@@ -3,8 +3,6 @@ package gift.configuration;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.web.client.RestTemplate;
 
 import java.time.Duration;
@@ -14,12 +12,10 @@ public class AppConfig {
 
     // 카카오 인증 관련 API 요청 템플릿
     @Bean
-    public RestTemplate KaKaoAuthRestTemplate(RestTemplateBuilder builder) {
+    public RestTemplate kakaoAuthRestTemplate(RestTemplateBuilder builder) {
         return builder
-                .rootUri("https://kauth.kakao.com/oauth")
                 .connectTimeout(Duration.ofSeconds(10))
                 .readTimeout(Duration.ofSeconds(10))
-                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE)
                 .build();
     }
 }
--- src/main/java/gift/infrastructure/KakaoAuthClient.java
+++ src/main/java/gift/infrastructure/KakaoAuthClient.java
@@ -3,52 +3,55 @@ package gift.infrastructure;
 import gift.dto.KakaoTokenDto;
 import gift.dto.KakaoUserInfoDto;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.RequestEntity;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.web.client.RestTemplate;
-
-import java.net.URI;
 
 @Service
 public class KakaoAuthClient {
     private final String kakaoRestApiKey;
     private final String redirectUri;
+    private final String clientSecret;
     private final RestTemplate restTemplate;
 
     public KakaoAuthClient(@Value("${kakao_rest_api_key}") String kakaoRestApiKey,
                             @Value("${redirect_uri}") String redirectUri,
+                            @Value("${client_secret}") String clientSecret,
                             RestTemplate kakaoAuthRestTemplate) {
         this.kakaoRestApiKey = kakaoRestApiKey;
         this.redirectUri = redirectUri;
+        this.clientSecret = clientSecret;
         this.restTemplate = kakaoAuthRestTemplate;
     }
 
     public KakaoTokenDto getKakaoToken(String code) {
-        final String url = "/token";
+        final String url = "https://kauth.kakao.com/oauth/token";
+
+        var headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
         var body = new LinkedMultiValueMap<String, String>();
         body.add("grant_type", "authorization_code");
         body.add("client_id", kakaoRestApiKey);
         body.add("redirect_uri", redirectUri);
         body.add("code", code);
+        body.add("client_secret", clientSecret);
 
-        var request = new RequestEntity<>(body, HttpMethod.POST, URI.create(url));
-        ResponseEntity<KakaoTokenDto> response = restTemplate.exchange(request, KakaoTokenDto.class);
+        var httpentity = new HttpEntity<>(body, headers);
 
+        ResponseEntity<KakaoTokenDto> response = restTemplate.exchange(url, HttpMethod.POST, httpentity, KakaoTokenDto.class);
         return response.getBody();
     }
 
     public KakaoUserInfoDto getKakaoUserInfo(KakaoTokenDto token) {
-        final String url = "/userinfo";
+        final String url = "https://kapi.kakao.com/v1/oidc/userinfo";
         var headers = new HttpHeaders();
         headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + token.accessToken());
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
-        var request = new RequestEntity<>(headers, HttpMethod.GET, URI.create(url));
-        ResponseEntity<KakaoUserInfoDto> response = restTemplate.exchange(request, KakaoUserInfoDto.class);
+        var httpentity = new HttpEntity<>(headers);
+        ResponseEntity<KakaoUserInfoDto> response = restTemplate.exchange(url, HttpMethod.GET, httpentity, KakaoUserInfoDto.class);
         return response.getBody();
     }
 }
